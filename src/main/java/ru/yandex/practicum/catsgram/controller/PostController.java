f4c4b262286f9acf2a5f2e5c3cf3bd1aafdf0f15
Golikov Aleksey <aleksey1991@mail.ru>
2023-01-25 08:48:18 +0300

    feat: added exceptions, services and PathVariable

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -1,27 +1,46 @@
 package ru.yandex.practicum.catsgram.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.model.Post;
+import ru.yandex.practicum.catsgram.service.PostService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class PostController {
+    private final PostService postService;
 
-    private final List<Post> posts = new ArrayList<>();
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
 
     @GetMapping("/posts")
-    public List<Post> findAll() {
-        return posts;
+    public List<Post> findAll(
+            @RequestParam(value = "page", defaultValue = "0", required = false) Integer page,
+            @RequestParam(value = "size", defaultValue = "10", required = false) Integer size,
+            @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
+
+
+        if(!(sort.equals("asc") || sort.equals("desc"))){
+            throw new IllegalArgumentException();
+        }
+        if(page < 0 || size <= 0){
+            throw new IllegalArgumentException();
+        }
+
+        Integer from = page * size;
+        return postService.findAll(size, from, sort);
     }
 
     @PostMapping(value = "/post")
     public Post create(@RequestBody Post post) {
-        posts.add(post);
-        return post;
+        return postService.create(post);
+    }
+
+    @GetMapping("/post/{postId}")
+    public Post findPost(@PathVariable("postId") Integer postId) {
+        return postService.findPostById(postId);
     }
 }
--- src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
@@ -1,13 +1,21 @@
-package ru.yandex.practicum.catsgram.controller;
+package ru.yandex.practicum.catsgram;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class SimpleController {
+    // создаём логер
+    private static final Logger log = LoggerFactory.getLogger(SimpleController.class);
 
     @GetMapping("/home")
     public String homePage() {
+        // логируем факт получения запроса
+        log.debug("Получен запрос GET /home.");
+
+        // возвращаем ответ в виде строки
         return "Котограм";
     }
 }
