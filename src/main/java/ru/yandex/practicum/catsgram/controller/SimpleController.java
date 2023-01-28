34b263ef6c4f908de802d7781b198ef64999387c
Golikov Aleksey <aleksey1991@mail.ru>
2023-01-28 09:13:35 +0300

    feat: RequestParam, ErrorHandler, AddFriends

--- src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.catsgram;
+package ru.yandex.practicum.catsgram.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,15 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SimpleController {
-    // создаём логер
     private static final Logger log = LoggerFactory.getLogger(SimpleController.class);
 
     @GetMapping("/home")
     public String homePage() {
-        // логируем факт получения запроса
         log.debug("Получен запрос GET /home.");
 
-        // возвращаем ответ в виде строки
         return "Котограм";
     }
 }
--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,20 +1,16 @@
 package ru.yandex.practicum.catsgram.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.model.User;
 import ru.yandex.practicum.catsgram.service.UserService;
 
 import java.util.Collection;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
-    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
