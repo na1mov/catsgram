16474493c02a18289a9ff4c320a11c1a3f08afba
Golikov Aleksey <aleksey1991@mail.ru>
2023-02-14 08:55:52 +0300

    refactor: added PostgreSQL as project DB

--- src/main/java/ru/yandex/practicum/catsgram/model/User.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -1,19 +1,32 @@
 package ru.yandex.practicum.catsgram.model;
 
-import java.time.LocalDate;
-import java.util.Objects;
-
 public class User {
-    private String email;
-    private String nickname;
-    private LocalDate birthdate;
 
-    public String getEmail() {
-        return email;
+    private String id;
+
+    private String username;
+
+    private String nickname;
+
+    public User() {
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public User(String id, String username, String nickname) {
+        this.id = id;
+        this.username = username;
+        this.nickname = nickname;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getNickname() {
@@ -24,24 +37,10 @@ public class User {
         this.nickname = nickname;
     }
 
-    public LocalDate getBirthdate() {
-        return birthdate;
+    public String getId() {
+        return id;
     }
 
-    public void setBirthdate(LocalDate birthdate) {
-        this.birthdate = birthdate;
-    }
+    ;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return email.equals(user.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(email);
-    }
 }
---
+++ src/main/sql/insert_into_cat_post.sql
@@ -0,0 +1,20 @@
+INSERT INTO public.cat_post (author_id, description, photo_url, creation_date)
+VALUES ('tom', 'first post with Tom', 'C:\\storage\catsgram\001.jpg', '2023-02-11 11:11:11');
+INSERT INTO public.cat_post (author_id, description, photo_url, creation_date)
+VALUES ('tom', 'second post with Tom', 'C:\\storage\catsgram\002.jpg', '2023-02-11 11:22:22');
+INSERT INTO public.cat_post (author_id, description, photo_url, creation_date)
+VALUES ('tom', 'third post with Tom', 'C:\\storage\catsgram\003.jpg', '2023-02-11 11:33:33');
+
+INSERT INTO public.cat_post (author_id, description, photo_url, creation_date)
+VALUES ('grumpy', 'first post with Grumpy', 'C:\\storage\catsgram\004.jpg', '2023-02-11 12:14:14');
+INSERT INTO public.cat_post (author_id, description, photo_url, creation_date)
+VALUES ('grumpy', 'second post with Grumpy', 'C:\\storage\catsgram\005.jpg', '2023-02-11 12:25:25');
+INSERT INTO public.cat_post (author_id, description, photo_url, creation_date)
+VALUES ('grumpy', 'third post with Grumpy', 'C:\\storage\catsgram\006.jpg', '2023-02-11 12:36:36');
+
+INSERT INTO public.cat_post (author_id, description, photo_url, creation_date)
+VALUES ('puss_in_boots', 'first post with puss_in_boots', 'C:\\storage\catsgram\007.jpg', '2023-02-11 13:17:17');
+INSERT INTO public.cat_post (author_id, description, photo_url, creation_date)
+VALUES ('puss_in_boots', 'first post with puss_in_boots', 'C:\\storage\catsgram\008.jpg', '2023-02-11 13:28:28');
+INSERT INTO public.cat_post (author_id, description, photo_url, creation_date)
+VALUES ('puss_in_boots', 'first post with puss_in_boots', 'C:\\storage\catsgram\009.jpg', '2023-02-11 13:39:39');
